e5bb99ba04af34b14d11965fa1199a850db03ecd
Skullywag <mjskull@gmail.com>
2016-08-12 18:26:20 +0100

    a14 update

---
+++ Source/BetterVents/PlaceWorker_OnWall.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+
+namespace BetterVents
+{
+
+    public class PlaceWorker_OnWall : PlaceWorker
+    {
+
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot)
+        {
+            IntVec3 c = loc;
+
+            Building support = c.GetEdifice();
+            if (support == null)
+            {
+                return (AcceptanceReport)("MessagePlacementOnSupport".Translate());
+            }
+
+            if (
+                (support.def == null) ||
+                (support.def.graphicData == null)
+            )
+            {
+                return (AcceptanceReport)("MessagePlacementOnSupport".Translate());
+            }
+
+            return (support.def.graphicData.linkFlags & (LinkFlags.Wall)) != 0
+                ? AcceptanceReport.WasAccepted
+                    : (AcceptanceReport)("MessagePlacementOnSupport".Translate());
+        }
+
+    }
+
+}
---
+++ Source/BetterVents/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BetterVents")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BetterVents")]
+[assembly: AssemblyCopyright("Copyright ©  2016 (skullywag)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8e081b6-8768-4fe9-817f-f0ee42cf13ef")]
+
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
