f6b3ffb240b32c5dfa5d92e187080baac2d7b4f0
Antaios <antaiozi@gmail.com>
2017-11-19 23:33:17 +0800

    B18 Update
    - Recompiled for B18
    - Adjusted PlaceWorker for new map handling
    - Added/Fixed Sprites for Closed Vents

--- Source/BetterVents/PlaceWorker_OnWall.cs
+++ Source/BetterVents/PlaceWorker_OnWall.cs
@@ -6,11 +6,11 @@ namespace BetterVents
     public class PlaceWorker_OnWall : PlaceWorker
     {
 
-        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Thing thingToIgnore = null)
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
         {
             IntVec3 c = loc;
 
-            Building support = c.GetEdifice(base.Map);
+            Building support = c.GetEdifice(map);
             if (support == null)
             {
                 return (AcceptanceReport)("MessagePlacementOnSupport".Translate());
